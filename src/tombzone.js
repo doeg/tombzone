6ad08b90f0f61f7df91cf615bde83ae3e369e0d7
Sara Bee <sara.bainbridge@gmail.com>
2015-09-04 11:49:26 -0400

    Add stuff to DOM, set up JS skeleton

--- src/tombzone.js
+++ src/tombzone.js
@@ -9,13 +9,42 @@
  *  Extremely spooky tombstones, all shapes and sizes.       /
  *  http://tomb.zone
  */
+
 (function tombzone() {
 
+    var templates = require("./templates");
 
+    var input;
+    var output;
 
+    var template;
+    var tombstone;
 
+    /**
+     * Update the tombstone whenever the template or tombstone text changes.
+     */
+    function onFormChange(e) {
+        parse();
+        draw();
+    }
+
+    function parse() {
+        console.log("parsing");
+    }
+
+    function draw() {
+        console.log("drawing");
+    }
 
     document.addEventListener("DOMContentLoaded", function(e) {
+
+        input = document.getElementById('input');
+        output = document.getElementById('output');
+
+        var formInputs = input.querySelectorAll('input,select');
+        for (i = 0; i < formInputs.length; i++) {
+           formInputs[i].onchange = onFormChange;
+        }
 
     });
 
--- tombzone-bundle.js
+++ tombzone-bundle.js
@@ -1,4 +1,33 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+module.exports = [
+    {
+        name: "weeping angel",
+        startRow: 11,
+        startCol: 12,
+        maxWidth: 16,
+        template: [
+            ['                  _  /)                   '],
+            ['                 mo / )                   '],
+            ['                 |/)\\)                    '],
+            ['                  /\\_                     '],
+            ['                  \\__|=                   '],
+            ['                 (    )                   '],
+            ['                 __)(__                   '],
+            ['           _____/      \\\\_____            '],
+            ['          |                  ||           '],
+            ['          |    † R.I.P. †    ||           '],
+            ['          |                  ||           '],
+            ['          | ################ ||           '],
+            ['          |                  ||           '],
+            ['  *       | *   **    * **   |**      **  '],
+            ['   \\))ejm97/.,(//,,..,,\\||(,,.,\\\\,.((//   ']
+          ],
+    }
+];
+
+},{}],2:[function(require,module,exports){
 /**
  *   ,                 ,,            ,
  *  ||                 ||           ||                        '
@@ -10,13 +39,42 @@
  *  Extremely spooky tombstones, all shapes and sizes.       /
  *  http://tomb.zone
  */
+
 (function tombzone() {
 
+    var templates = require("./templates");
 
+    var input;
+    var output;
 
+    var template;
+    var tombstone;
 
+    /**
+     * Update the tombstone whenever the template or tombstone text changes.
+     */
+    function onFormChange(e) {
+        parse();
+        draw();
+    }
+
+    function parse() {
+        console.log("parsing");
+    }
+
+    function draw() {
+        console.log("drawing");
+    }
 
     document.addEventListener("DOMContentLoaded", function(e) {
+
+        input = document.getElementById('input');
+        output = document.getElementById('output');
+
+        var formInputs = input.querySelectorAll('input,select');
+        for (i = 0; i < formInputs.length; i++) {
+           formInputs[i].onchange = onFormChange;
+        }
 
     });
 
@@ -24,4 +82,4 @@
 
 
 
-},{}]},{},[1]);
+},{"./templates":1}]},{},[2]);
