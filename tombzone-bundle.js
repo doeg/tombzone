5fb4c6e27d166fc2ce7bd7e94f23e8834bc10e90
Sara Bee <sara.bainbridge@gmail.com>
2015-09-04 12:05:43 -0400

    More styling for the output container

--- tombzone-bundle.js
+++ tombzone-bundle.js
@@ -42,12 +42,16 @@ module.exports = [
 
 (function tombzone() {
 
+    "use strict"
+
     var templates = require("./templates");
 
     var input;
     var output;
+    var epitaphInput;
 
     var template;
+    // var epitaph;
     var tombstone;
 
     /**
@@ -59,25 +63,37 @@ module.exports = [
     }
 
     function parse() {
+        console.log();
         console.log("parsing");
+        console.log("~~~~~~~");
+
+        var epitaph = epitaphInput.value;
+        console.log(epitaph);
+
     }
 
     function draw() {
+        console.log();
         console.log("drawing");
+        console.log("~~~~~~~");
     }
 
     document.addEventListener("DOMContentLoaded", function(e) {
 
-        input = document.getElementById('input');
-        output = document.getElementById('output');
+        input = document.getElementById("input");
+        output = document.getElementById("output");
 
-        var formInputs = input.querySelectorAll('input,select');
-        for (i = 0; i < formInputs.length; i++) {
+        // Bind change listener to ALL form inputs
+        var formInputs = input.querySelectorAll("input,select,textarea");
+        for (var i = 0; i < formInputs.length; i++) {
            formInputs[i].onchange = onFormChange;
         }
 
+        // Make sure we bind to both keyup and onchange for the epitaph text
+        epitaphInput = document.getElementById("epitaph");
+        epitaphInput.onkeyup = onFormChange;
+        epitaphInput.onblur = onFormChange;
     });
-
 })();
 
 
